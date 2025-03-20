9472f4183e24fcd33900e4aca75d0755489b2ec7
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-20 02:06:20 -0700

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addCart } from "../redux/action";
-
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const Products = () => {
   const [data, setData] = useState([]);
-  const [filter, setFilter] = useState(data);
+  const [filter, setFilter] = useState([]);
   const [loading, setLoading] = useState(false);
   let componentMounted = true;
 
@@ -18,18 +16,19 @@ const Products = () => {
 
   const addProduct = (product) => {
     dispatch(addCart(product));
+    toast.success("Added to cart");
   };
 
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
-      const response = await fetch("https://fakestoreapi.com/products/");
+      const response = await fetch("/products.json"); // Fetch from public folder
       if (componentMounted) {
-        setData(await response.clone().json());
-        setFilter(await response.json());
+        const products = await response.json();
+        setData(products.products); // Access the "products" array
+        setFilter(products.products);
         setLoading(false);
       }
-
       return () => {
         componentMounted = false;
       };
@@ -44,30 +43,17 @@ const Products = () => {
         <div className="col-12 py-5 text-center">
           <Skeleton height={40} width={560} />
         </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
+        {[...Array(6)].map((_, index) => (
+          <div key={index} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+            <Skeleton height={592} />
+          </div>
+        ))}
       </>
     );
   };
 
   const filterProduct = (cat) => {
-    const updatedList = data.filter((item) => item.category === cat);
+    const updatedList = data.filter((item) => item.category.includes(cat));
     setFilter(updatedList);
   };
 
@@ -75,103 +61,68 @@ const Products = () => {
     return (
       <>
         <div className="buttons text-center py-5">
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => setFilter(data)}
-          >
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => setFilter(data)}>
             All
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("men's clothing")}
-          >
-            Men's Clothing
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("sneakers")}>
+            Sneakers
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("women's clothing")}
-          >
-            Women's Clothing
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("casual")}>
+            Casual
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("jewelery")}
-          >
-            Jewelery
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("formal")}>
+            Formal
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("electronics")}
-          >
-            Electronics
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("children")}>
+            Children's
           </button>
         </div>
 
-        {filter.map((product) => {
-          return (
-            <div
-              id={product.id}
-              key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
-            >
-              <div className="card text-center h-100" key={product.id}>
-                <img
-                  className="card-img-top p-3"
-                  src={product.image}
-                  alt="Card"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {product.title.substring(0, 12)}...
-                  </h5>
-                  <p className="card-text">
-                    {product.description.substring(0, 90)}...
-                  </p>
-                </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">$ {product.price}</li>
-                  {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-                </ul>
-                <div className="card-body">
-                  <Link
-                    to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
-                  >
-                    Buy Now
-                  </Link>
-                  <button
-                    className="btn btn-dark m-1"
-                    onClick={() => {
-                      toast.success("Added to cart");
-                      addProduct(product);
-                    }}
-                  >
-                    Add to Cart
-                  </button>
-                </div>
+        {filter.map((product) => (
+          <div key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+            <div className="card text-center h-100">
+              <img
+                className="card-img-top p-3"
+                src={product.imageUrl}
+                alt={product.name}
+                height={300}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.name.substring(0, 12)}...</h5>
+                <p className="card-text">{product.description.substring(0, 90)}...</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item lead">
+                  {product.price.toLocaleString()} {product.currency}
+                </li>
+              </ul>
+              <div className="card-body">
+                <Link to={`/product/${product.id}`} className="btn btn-dark m-1">
+                  Buy Now
+                </Link>
+                <button className="btn btn-dark m-1" onClick={() => addProduct(product)}>
+                  Add to Cart
+                </button>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </>
     );
   };
+
   return (
-    <>
-      <div className="container my-3 py-3">
-        <div className="row">
-          <div className="col-12">
-            <h2 className="display-5 text-center">Latest Products</h2>
-            <hr />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          {loading ? <Loading /> : <ShowProducts />}
+    <div className="container my-3 py-3">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="display-5 text-center">Latest Footwear</h2>
+          <hr />
         </div>
       </div>
-    </>
+      <div className="row justify-content-center">
+        {loading ? <Loading /> : <ShowProducts />}
+      </div>
+    </div>
   );
 };
 
