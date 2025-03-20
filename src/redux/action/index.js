8b49bc829741d29d8e21d0fb38c1ac4d59fea279
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-20 06:07:30 -0700

    Update index.js

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,15 +1,38 @@
 // For Add Item to Cart
-export const addCart = (product) =>{
-    return {
-        type:"ADDITEM",
-        payload:product
-    }
-}
+export const addCart = (product) => {
+  return {
+    type: "ADDITEM",
+    payload: product,
+  };
+};
 
-// For Delete Item to Cart
-export const delCart = (product) =>{
-    return {
-        type:"DELITEM",
-        payload:product
-    }
-}
+// For Delete Item from Cart
+export const delCart = (product) => {
+  return {
+    type: "DELITEM",
+    payload: product,
+  };
+};
+
+// For Clear Cart
+export const clearCart = () => {
+  return {
+    type: "CLEARCART",
+  };
+};
+
+// For Save Item for Later
+export const saveForLater = (product) => {
+  return {
+    type: "SAVEFORLATER",
+    payload: product,
+  };
+};
+
+// For Add Item Back to Cart from Saved
+export const addBackToCart = (product) => {
+  return {
+    type: "ADDBACKTOCART",
+    payload: product,
+  };
+};
