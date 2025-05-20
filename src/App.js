e816d9c81a6a49dbbdf74fc27123ad4d051a2bcc
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-21 00:13:51 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { AuthProvider } from "./AuthContext";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </Provider>
+  );
+};
+
+export default App;
